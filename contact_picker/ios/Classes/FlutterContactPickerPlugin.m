6840091cb7c6344d39f1a2fb4f0c14c4c2f67dce
Chandra Abdul Fattah <chandrashibezzo@gmail.com>
2025-10-15 13:27:35 +0700

    Adding plus on .m file

--- contact_picker/ios/Classes/FlutterContactPickerPlugin.m
+++ contact_picker/ios/Classes/FlutterContactPickerPlugin.m
@@ -1,11 +1,11 @@
 #import "FlutterContactPickerPlugin.h"
-#if __has_include(<fluttercontactpicker/fluttercontactpicker-Swift.h>)
-#import <fluttercontactpicker/fluttercontactpicker-Swift.h>
+#if __has_include(<fluttercontactpicker_plus/fluttercontactpicker_plus-Swift.h>)
+#import <fluttercontactpicker_plus/fluttercontactpicker_plus-Swift.h>
 #else
 // Support project import fallback if the generated compatibility header
 // is not copied when this plugin is created as a library.
 // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "fluttercontactpicker-Swift.h"
+#import "fluttercontactpicker_plus-Swift.h"
 #endif
 
 @implementation FlutterContactPickerPlugin : NSObject 
